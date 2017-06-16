b34cbeaa0e7808e109f05ac3fe39d85b60ec44b4
Franco Rabaglia <francoo.hd@gmail.com>
2017-06-16 16:27:09 -0300

    .

--- src/constants/actions/AuthorResult/index.js
+++ src/constants/actions/AuthorResult/index.js
@@ -2,9 +2,25 @@ import {
   AUTHOR_RESULT_REQUEST,
   AUTHOR_RESULT_SUCCESS
 } from '../../action-types/AuthorResult'
+import {
+  ERROR
+} from '../../action-types/GlobalMesssages'
+import {
+  API
+} from '../../action-types/API'
+import {
+  SyllabusHTTPService
+} from '../../../os-toolkit/SyllabusHTTPService'
+
 export const authorResultRequest = () => {
   return {
-    type: AUTHOR_RESULT_REQUEST
+    type: API,
+    payload: {
+      request: 'recipes.json',
+      pending: AUTHOR_RESULT_REQUEST,
+      success: AUTHOR_RESULT_SUCCESS,
+      error: ERROR
+    }
   }
 }
 
