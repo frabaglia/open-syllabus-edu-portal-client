6ba811c4a84e89fdba6067ba31ecdd6ea452228b
Esteban Erbes <fedeerbes@gmail.com>
2017-07-03 20:39:56 -0300

    login and reconver-login

--- src/components/Views/Login/component.js
+++ src/components/Views/Login/component.js
@@ -4,8 +4,8 @@ import SVGEduPortalLogo from '../../Dummy/SVG/EduPortalLogo/component'
 import SVGCloseIcon from '../../Dummy/SVG/CloseIcon/component'
 import SVGUserIcon from '../../Dummy/SVG/UserIcon/component'
 import SVGKeyIcon from '../../Dummy/SVG/KeyIcon/component'
-import SVGCheckIcon from '../../Dummy/SVG/CheckIcon/component'
 import ButtonLabel from '../../Dummy/Buttons/ButtonLabel/component'
+import {Link} from 'react-router';
 
 import './component.sass'
 
@@ -15,14 +15,14 @@ class DummyLogin extends Component {
     super();
     this.state={
       renderMessage: false,
-      recoverState: false,
-      recoverSuccess: false
+      email: '',
+      password:''
     }
   }
 
   login = () =>{
-    console.log('login');
-    this.props.router.push('/university-portal')
+    if(this.state.email === 'EDU' && this.state.password === 'EDUPASS') this.props.router.push('/university-portal')
+    else this.setState({renderMessage:true})
   }
 
   recoverPassword = ( ) =>{
@@ -33,145 +33,25 @@ class DummyLogin extends Component {
     this.props.router.push('/')
   }
 
+  closeWarning = () =>{this.setState({renderMessage:false})}
+
   renderMessage = () =>{
     return (this.state.renderMessage) ?
     (
       <div className="message">
-        {SVGCloseIcon({color:'#F7485D'})}
+        {SVGCloseIcon({backgroundColor:'#F7485D', color:'#FFFFFF', click:this.closeWarning})}
         <p>Please check your Email and Password</p>
       </div>
     ):
     (<div></div>)
   }
 
-  renderBody = () =>{
-    let component;
-    if(this.state.recoverState){
-      component =(
-        <div>
-          <div className="login-body">
-            <p className="login-body-title">
-              Use your Email to recover your password
-            </p>
-            <div className="login-body-inputs">
-              <div className="input-text">
-                <input type="text" placeholder="Email"/>
-                {SVGUserIcon()}
-              </div>
-            </div>
-            <div className="login-body-message"></div>
-            <div className="login-body-button">
-              {ButtonLabel({
-                backgroundColor:'#9BD331',
-                color:'#FFFFFF',
-                border: '#9BD331',
-                title: 'RECOVER',
-                click: this.recoverPassword()
-              })}
-            </div>
-          </div>
-          <div className="login-footer">
-            <p>Back to login</p>
-          </div>
-        </div>
-      )
-
-      if(this.state.recoverSuccess){
-        component = (
-          <div>
-            <div className="login-body">
-              <div className="login-body-success">
-                <div className="login-body-success-icon">
-                  {SVGCheckIcon()}
-                </div>
-                <div className="login-body-success-description">
-                  Good news, we have sent you a new password, check your inbox.
-                </div>
-              </div>
-            </div>
-          </div>
-        )
-      }
-    }
-    else {
-      component = (
-        <div>
-          <div className="login-body">
-            <div className="login-body-inputs">
-              <div className="input-text">
-                <input type="text" placeholder="Email"/>
-                {SVGUserIcon()}
-              </div>
-              <div className="input-password">
-                <input type="password" placeholder="Password"/>
-                {SVGKeyIcon()}
-              </div>
-            </div>
-            <div className="login-body-message">
-              {this.renderMessage()}
-            </div>
-            <div className="login-body-button">
-              {ButtonLabel({
-                backgroundColor:'#9BD331',
-                color:'#FFFFFF',
-                border: '#9BD331',
-                title: 'LOGIN',
-                click: this.login
-              })}
-            </div>
-          </div>
-          <div className="login-footer">
-            <p>Forgot your password?</p>
-          </div>
-        </div>
-      )
-    }
-
-    return component;
-  }
+  handleChangeInput = (type, event) => this.setState({[`${type}`]:event.target.value})
 
   render() {
     return (
         <div className="fixed-container">
           <div className="centered-login" style={{position:'relative'}}>
-            <div style={{position:'absolute',top:'0',left:'0'}}>
-              <span
-                style={{display:'block',cursor:'pointer'}}
-                onClick={()=>{this.setState({
-                  renderMessage: false,
-                  recoverState:false,
-                  recoverSuccess:false
-                })}}
-                >Init Login
-              </span>
-              <span
-                style={{display:'block',cursor:'pointer'}}
-                onClick={()=>{this.setState({
-                  renderMessage: true,
-                  recoverState:false,
-                  recoverSuccess:false
-                })}}
-                >Wrong pass or user
-              </span>
-              <span
-                style={{display:'block',cursor:'pointer'}}
-                onClick={()=>{this.setState({
-                  renderMessage: false,
-                  recoverState:true,
-                  recoverSuccess:false
-                })}}
-                >Recover pass
-              </span>
-              <span
-                style={{display:'block',cursor:'pointer'}}
-                onClick={()=>{this.setState({
-                  renderMessage: false,
-                  recoverState:true,
-                  recoverSuccess:true
-                })}}
-                >Success recover
-              </span>
-            </div>
             <div className="login">
               <div className="login-close">
                 {SVGCloseIcon({color:'#FFFFFF',backgroundColor:'#C8CFD7' , click: this.closeLogin})}
@@ -179,7 +59,35 @@ class DummyLogin extends Component {
               <div className="login-header">
                 {SVGEduPortalLogo({color:'#9BD331', colorEDU:'#3B475B'})}
               </div>
-              {this.renderBody()}
+              <div className="login-body">
+                <div className="login-body-inputs">
+                  <div className="input-text">
+                    <input type="text" placeholder="Email: use EDU" onChange={(e) => {this.handleChangeInput('email',e)}}/>
+                    {SVGUserIcon()}
+                  </div>
+                  <div className="input-password">
+                    <input type="password" placeholder="Password: use EDUPASS" onChange={(e) => {this.handleChangeInput('password',e)}}/>
+                    {SVGKeyIcon()}
+                  </div>
+                </div>
+                <div className="login-body-message">
+                  {this.renderMessage()}
+                </div>
+                <div className="login-body-button">
+                  {ButtonLabel({
+                    backgroundColor:'#9BD331',
+                    color:'#FFFFFF',
+                    border: '#9BD331',
+                    title: 'LOGIN',
+                    click: this.login
+                  })}
+                </div>
+              </div>
+              <div className="login-footer">
+                <Link to="/recover-password">
+                    <p>Forgot your password?</p>
+                </Link>
+              </div>
             </div>
           </div>
         </div>
